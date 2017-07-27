b873b8528cd392b9eaba9446711b273526029a3e
leiyongbo <lyb19900227@126.com>
2017-07-27 17:42:35 +0800

    reverse-words-in-a-string

--- interview/lintcode/reverse-words-in-a-string.py
+++ interview/lintcode/reverse-words-in-a-string.py
@@ -3,4 +3,21 @@
 # @User: leiyongbo <lyb19900227@126.com>
 # @Date: 2017/7/27
 # @Time: 11:45
-# @Description: 
+# @url: http://www.lintcode.com/zh-cn/problem/binary-tree-maximum-node/
+# @Description:
+
+class Solution:
+    # @param s : A string
+    # @return : A string
+    def reverseWords(self, s):
+        # write your code here
+        if not s:
+            return s
+        str_list = filter(lambda t: t != "", s.strip().split(" "))
+        str_list.reverse()
+        return " ".join(str_list)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print (s.reverseWords("How    are you?"))
